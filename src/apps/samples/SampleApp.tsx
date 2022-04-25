575cb634033c57d7e02117fd92bc8fe3edc81dc9
ketyung <ketyung@techchee.com>
2022-04-25 11:04:10 +0800

    update

--- src/apps/samples/SampleApp.tsx
+++ src/apps/samples/SampleApp.tsx
@@ -1,3 +1,8 @@
 import { FC } from "react";
+import { MenuBar } from "../../common/views/menu/MenuBar";
+import '../../common/css/view.css';
 
-
+export const SampleApp : FC = () => {
+   
+    return <><MenuBar/></>
+};
